a71d408a24ca6518892f0a2d580873b3c8bb2395
ramprakash.v <ramprakash.v@juspay.in>
2024-05-27 16:52:23 +0530

    Added dotnet framework

---
+++ dotnet_framework_backend_kit/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SmartGatewayDotnetBackendApiKeyKit
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();
+            routes.MapRoute(
+                name: "CustomFileRoute",
+                url: "{fileName}",
+                defaults: new { controller = "File", action = "Index" } // Assuming FileController with Index action
+            );
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "File", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ dotnet_framework_backend_kit/Models/RefundStatusViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SmartGatewayDotnetBackendApiKeyKit.Models
+{
+    public class RefundStatusViewModel
+    {
+        public Dictionary<string, string> Refund { get; set; }
+
+        public string StringifiedRefund { get; set; }
+
+    }
+}
